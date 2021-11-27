cb2df4d0b6488478d8ef5b979177afe5407d9e31
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-11-27 17:21:24 +0200

    solution was added

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -14,9 +14,9 @@ public class Counter {
 
     public void decreaseValue() {
         logger.info(String.format(MESSAGE,
-                "Before decrementing", Thread.currentThread().getName(), value));
+                    "Before decrementing", Thread.currentThread().getName(), value));
         value--;
         logger.info(String.format(MESSAGE,
-                "After decrementing", Thread.currentThread().getName(), value));
+                    "After decrementing", Thread.currentThread().getName(), value));
     }
 }
