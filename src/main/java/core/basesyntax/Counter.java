318339e03e8d43376878e422824ed46cc5aa1964
Nazar Hetun <nazarhetun@gmail.com>
2021-12-07 12:03:45 +0200

    fix race condition

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -13,9 +13,11 @@ public class Counter {
     }
 
     public void decreaseValue() {
-        logger.info(String.format(MESSAGE,
-                "Before decrementing", Thread.currentThread().getName(), value));
+        synchronized(this) {
+            logger.info(String.format(MESSAGE,
+                    "Before decrementing", Thread.currentThread().getName(), value));
         value--;
+        }
         logger.info(String.format(MESSAGE,
                 "After decrementing", Thread.currentThread().getName(), value));
     }
