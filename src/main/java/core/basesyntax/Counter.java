8e3de71255d4516308fcdf54dd97ac2536f4e0ad
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2022-01-18 16:32:01 +0300

    corrections after mentor review

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -12,12 +12,10 @@ public class Counter {
         this.value = value;
     }
 
-    public void decreaseValue() {
+    public synchronized void decreaseValue() {
         logger.info(String.format(MESSAGE,
                 "Before decrementing", Thread.currentThread().getName(), value));
-        synchronized (this) {
             value--;
-        }
         logger.info(String.format(MESSAGE,
                 "After decrementing", Thread.currentThread().getName(), value));
     }
