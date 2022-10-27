7006e9f160fd359eb15ac24eeca5c95094c25f66
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-27 23:43:29 +0300

    refactor

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -7,6 +7,7 @@ public class Counter {
     private static final Logger logger = LogManager.getLogger(Counter.class);
     private static final String MESSAGE = "%20s, Thread # %2s, counter value %2d";
     private int value;
+
     public Counter(int value) {
         this.value = value;
     }
