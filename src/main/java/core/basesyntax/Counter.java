4370892e315ac5220e02d19607045a2b3c10d7a8
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-27 23:38:44 +0300

    add log

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -7,7 +7,6 @@ public class Counter {
     private static final Logger logger = LogManager.getLogger(Counter.class);
     private static final String MESSAGE = "%20s, Thread # %2s, counter value %2d";
     private int value;
-
     public Counter(int value) {
         this.value = value;
     }
