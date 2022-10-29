84c0b85e8348b4ba0d36a8821fa4715a16d8fc9d
Anna Popova <anitapopova1996@gmail.com>
2022-10-29 16:01:40 +0300

    fixed

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -13,14 +13,10 @@ public class Counter {
     }
 
     public synchronized void decreaseValue() {
-            logger.info(String.format(MESSAGE,
-                    "Before decrementing", Thread.currentThread().getName(), value));
-            value--;
-            logger.info(String.format(MESSAGE,
-                    "After decrementing", Thread.currentThread().getName(), value));
-    }
-
-    public synchronized int getValue() {
-        return value;
+        logger.info(String.format(MESSAGE,
+                "Before decrementing", Thread.currentThread().getName(), value));
+        value--;
+        logger.info(String.format(MESSAGE,
+                "After decrementing", Thread.currentThread().getName(), value));
     }
 }
