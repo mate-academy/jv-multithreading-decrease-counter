ce0139498fb4832266df6e9f20d00fca7758af82
RomanPlk <roman.pylypchuk@yahoo.com>
2023-03-23 18:01:49 +0200

    solution

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -12,11 +12,11 @@ public class Counter {
         this.value = value;
     }
 
-    public void decreaseValue() {
-        logger.info(String.format(MESSAGE,
-                "Before decrementing", Thread.currentThread().getName(), value));
-        value--;
-        logger.info(String.format(MESSAGE,
-                "After decrementing", Thread.currentThread().getName(), value));
+    public synchronized void decreaseValue() {
+            logger.info(String.format(MESSAGE,
+                    "Before decrementing", Thread.currentThread().getName(), value));
+            value--;
+            logger.info(String.format(MESSAGE,
+                    "After decrementing", Thread.currentThread().getName(), value));
     }
 }
