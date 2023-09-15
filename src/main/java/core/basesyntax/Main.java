dd5135543fa98f9780886c1237ab8db85ad99153
Anton Uzhva <anton.itec91@gmail.com>
2023-09-16 00:55:38 +0300

    Complete task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter(20);
         for (int i = 1; i <= 20; i++) {
-            new MyThread(counter, String.valueOf(i)).start();
+            new MyThread(counter, String.valueOf(i))
+                    .start();
         }
     }
 }
