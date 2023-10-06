27eaf0db03a4e325915a28ad90eebbe3927c12de
vasyl.moshun <vasyl.moshun@gmail.com>
2023-10-06 13:07:28 +0300

    done

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,15 +1,26 @@
 package core.basesyntax;
 
+import java.util.concurrent.Semaphore;
+
 public class MyThread extends Thread {
     private final Counter counter;
+    private final Semaphore semaphore;
 
-    public MyThread(Counter counter, String name) {
+
+    public MyThread(Counter counter, String name, Semaphore semaphore) {
         super(name);
         this.counter = counter;
+        this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+                throw new RuntimeException("Something went wrong", e);
+        }
         counter.decreaseValue();
+        semaphore.release();
     }
 }
