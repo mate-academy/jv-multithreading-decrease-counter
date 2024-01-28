47fb0361add1fca75abee3e2a2446de41a5243b6
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-01-28 15:44:29 +0200

    added solved task

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -3,20 +3,23 @@ package core.basesyntax;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
     private static final Logger logger = LogManager.getLogger(Counter.class);
     private static final String MESSAGE = "%20s, Thread # %2s, counter value %2d";
-    private int value;
+//    private int value;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public Counter(int value) {
-        this.value = value;
+        this.value.set(value);
     }
 
     public void decreaseValue() {
         logger.info(String.format(MESSAGE,
-                "Before decrementing", Thread.currentThread().getName(), value));
-        value--;
+                "Before decrementing", Thread.currentThread().getName(), value.get()));
+        value.decrementAndGet();
         logger.info(String.format(MESSAGE,
-                "After decrementing", Thread.currentThread().getName(), value));
+                "After decrementing", Thread.currentThread().getName(), value.get()));
     }
 }
