aca39aa8de2401d7e3f31256b9dd44d348563e3a
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-01-28 17:03:22 +0200

    added solved task_repaired

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Counter {
     private static final Logger logger = LogManager.getLogger(Counter.class);
     private static final String MESSAGE = "%20s, Thread # %2s, counter value %2d";
-//    private int value;
+    //    private int value;
     private AtomicInteger value = new AtomicInteger(0);
 
     public Counter(int value) {
