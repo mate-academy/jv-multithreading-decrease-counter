d0446fb9c1611a6d2880cdc3d3e5883ffd6bc0f7
Dmitri Kolos <skil@bigmir.net>
2024-02-02 22:12:25 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
         this.value = value;
     }
 
-    public  synchronized void decreaseValue() {
+    public synchronized void decreaseValue() {
         logger.info(String.format(MESSAGE,
                 "Before decrementing", Thread.currentThread().getName(), value));
         value--;
