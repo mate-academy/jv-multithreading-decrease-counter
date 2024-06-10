e004d5197e78121a1ce4f118d679eae77b55ba16
azzilian <crusotdeath@gmail.com>
2024-06-10 16:15:00 +0300

    Update - add synchronized monitor

--- src/main/java/core/basesyntax/Counter.java
+++ src/main/java/core/basesyntax/Counter.java
@@ -6,17 +6,27 @@ import org.apache.logging.log4j.Logger;
 public class Counter {
     private static final Logger logger = LogManager.getLogger(Counter.class);
     private static final String MESSAGE = "%20s, Thread # %2s, counter value %2d";
+
     private int value;
+
+    private Object monitor = new Object();
 
     public Counter(int value) {
         this.value = value;
     }
 
     public void decreaseValue() {
-        logger.info(String.format(MESSAGE,
-                "Before decrementing", Thread.currentThread().getName(), value));
-        value--;
-        logger.info(String.format(MESSAGE,
-                "After decrementing", Thread.currentThread().getName(), value));
+        synchronized (monitor) {
+            logger.info(String.format(MESSAGE,
+                    "Before decrementing", Thread.currentThread().getName(), value));
+            try {
+                value--;
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Cannot lower value" + e);
+            }
+            logger.info(String.format(MESSAGE,
+                    "After decrementing", Thread.currentThread().getName(), value));
+        }
     }
 }
