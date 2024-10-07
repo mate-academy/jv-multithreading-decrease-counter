1b12c7ba5842b573220ea9acb5979d8a7db3944e
mgucma <marek.gucma@gmail.com>
2024-10-07 09:29:56 +0200

    Add solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,15 +1,18 @@
 package core.basesyntax;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MyThread extends Thread {
+    private static final Logger logger = LogManager.getLogger(MyThread.class);
     private final Counter counter;
 
-    public MyThread(Counter counter, String name) {
-        super(name);
+    public MyThread(Counter counter) {
         this.counter = counter;
     }
 
     @Override
     public void run() {
-        counter.decreaseValue();
+        counter.decrement();
     }
 }
