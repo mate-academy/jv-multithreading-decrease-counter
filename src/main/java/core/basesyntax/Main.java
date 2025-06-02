6ad2083ef87435fbe7f57d09650e563de0907fbb
MishaHMK <gumeniukmich@gmail.com>
2025-06-02 22:57:31 +0300

    task completed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,17 @@
 package core.basesyntax;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter(20);
         for (int i = 1; i <= 20; i++) {
             new MyThread(counter, String.valueOf(i)).start();
+
+            //Solution #2
+            /*
+                MyThread myThread = new MyThread(counter, String.valueOf(i));
+                myThread.start();
+                myThread.join();
+             */
         }
     }
 }
